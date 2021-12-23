2e3c9b9c8e45ab9351d94cde7629db203118227b
PavelZ <r3volut1oner@gmail.com>
2021-12-23 22:19:02 +0200

    Add test for PublicFromPrivate key

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -36,3 +36,39 @@ func TestKey_Check(t *testing.T) {
 
 	assert.Equal(t, 372, times)
 }
+
+func TestPublicFromPrivate(t *testing.T) {
+	file, err := os.Open("./fixtures/public_from_private.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	times := 0
+	lineNumber := 1
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.Fields(scanner.Text())
+
+		privateKeyBytes, _ := hex.DecodeString(line[0])
+		expectedSuccess, _ := strconv.ParseBool(line[1])
+
+		privateKey, _ := KeyFromBytes(privateKeyBytes)
+		actualPublicKey, err := PublicFromPrivate(privateKey)
+
+		assert.Equal(t, expectedSuccess, err == nil, fmt.Sprintf("failed at line: %d", lineNumber))
+
+		if len(line) > 2 {
+			expectedBytes, _ := hex.DecodeString(line[2])
+			expectedPublicKey, _ := KeyFromBytes(expectedBytes)
+
+			assert.Equal(t, expectedPublicKey, actualPublicKey, fmt.Sprintf("failed at line: %d", lineNumber))
+		}
+
+		lineNumber++
+		times++
+	}
+
+	assert.Equal(t, 272, times)
+}
